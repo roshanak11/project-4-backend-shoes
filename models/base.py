252a379086ec3337f2437f716648ce6b148247d7
roshanak <roshanak.ahmed11@gmail.com>
2022-08-18 00:22:29 +0600

    indentation

--- models/base.py
+++ models/base.py
@@ -11,19 +11,21 @@ from datetime import *
 # * that EVERY model will use.
 class BaseModel:
 
-  # ! This is a static field. I don't need to make an object to set this value.
-  # ! It's a way of specifying fields that don't change in value.
-  id = db.Column(db.Integer, primary_key=True)
+    # ! This is a static field. I don't need to make an object to set this value.
+    # ! It's a way of specifying fields that don't change in value.
+    id = db.Column(db.Integer, primary_key=True)
 
-  created_at = db.Column(db.DateTime, default=datetime.utcnow)
-  updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
-  # ! Add a method here to save my model to the database
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
+    # ! Add a method here to save my model to the database
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-  # ! Add a method to remove
-  def remove(self):
-      db.session.delete(self)
-      db.session.commit
+    # ! Add a method to remove
+    def remove(self):
+        db.session.delete(self)
+        db.session.commit
