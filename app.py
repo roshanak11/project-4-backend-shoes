44c8d60ac3a9c2ae3e79d3d2876fdca880932e5b
roshanak <roshanak.ahmed11@gmail.com>
2022-08-18 00:11:54 +0600

    debugging

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 # ! import Bcrypt.
 from flask_bcrypt import Bcrypt
-
-...
 from flask_cors import CORS
-...
 
 from config.environment import db_URI
 
@@ -15,6 +12,7 @@ from config.environment import db_URI
 # ? where you run flask from. If you run this directly,
 # ? it will be '__main__'
 app = Flask(__name__)
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -30,7 +28,3 @@ from controllers import shoes, categories, users
 app.register_blueprint(shoes.router, url_prefix="/api")
 app.register_blueprint(categories.router, url_prefix="/api")
 app.register_blueprint(users.router, url_prefix="/api")
-
-
-# Deployment
-CORS(app)
