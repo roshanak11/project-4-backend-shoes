1732b1d4df6a6187543b91072f8abcc1986e8d58
roshanak <roshanak.ahmed11@gmail.com>
2022-08-17 21:58:21 +0600

    pink croc update

--- config/environment.py
+++ config/environment.py
@@ -1,2 +1,4 @@
-db_URI = "postgresql://localhost:5432/shoes_db"
-secret = "mysecret321"
+import os
+
+db_URI = os.getenv('DATABASE_URL', 'postgresql://localhost:5432/shoes_db')
+secret = os.getenv('SECRET', 'a suitable secret')
